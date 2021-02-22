4d86cdb4ea35a9339a094375d94dc0c7507d7a0e
lukman <nandes88.ni@gmail.com>
2021-02-23 00:00:06 +0700

    menambahkan items

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
       black: colors.black,
       white: colors.white,
       gray: { ...colors.coolGray, 100: "#f9f9f9", 200: "#F1F1F1" },
-      red: colors.red,
+      red: { ...colors.red, 600: "#E0195D" },
       yellow: colors.amber,
       green: colors.emerald,
       blue: {
